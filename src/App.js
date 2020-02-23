766ffcfc3b95f74505028553208914dc3613a067
sofiafpdferreira <57071456+sofiafpdferreira@users.noreply.github.com>
2020-02-23 12:23:59 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Open source code
-        <a
-          href="https://github.com/sofiafpdferreira/final-project"
-          target="_blank"
-        >
-          {""} link {""}
-        </a>
-        by
-        <a href="https://pt.linkedin.com/in/sofia-franco-ferreira-17822b170">
-          {" "}
-          Sofia F. Ferreira
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App </h1>
+        <footer>
+          Open source code
+          <a
+            href="https://github.com/sofiafpdferreira/final-project"
+            target="_blank"
+          >
+            {""} link {""}
+          </a>
+          by
+          <a href="https://pt.linkedin.com/in/sofia-franco-ferreira-17822b170">
+            {" "}
+            Sofia F. Ferreira
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
