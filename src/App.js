8c711ab68b0316aa9cd2bb3b2fc1d29647c096f7
sofiafpdferreira <57071456+sofiafpdferreira@users.noreply.github.com>
2020-02-23 12:04:05 +0000

    added code link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Open source code
+        <a
+          href="https://github.com/sofiafpdferreira/final-project"
+          target="_blank"
+        >
+          {""} link {""}
+        </a>
+        by
+        <a href="https://pt.linkedin.com/in/sofia-franco-ferreira-17822b170">
+          {" "}
+          Sofia F. Ferreira
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
