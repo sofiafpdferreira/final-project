9a500c7c983e8dd28971c314701a14285adca18f
sofiafpdferreira <57071456+sofiafpdferreira@users.noreply.github.com>
2020-02-25 20:38:37 +0000

    remove precipitation

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -20,7 +20,6 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: {props.data.precipitation} %</li>
             <li>Humidity: {props.data.humidity} %</li>
             <li>Wind: {props.data.wind} Km/h</li>
           </ul>
